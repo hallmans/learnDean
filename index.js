180c96bf9b7acd6fcd676c0075d57ffb0e04816e
Dean Hallman <dean@foresight.dev>
2021-01-21 16:43:04 -0500

    Initial version of learning

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var cowsay = require("cowsay");
+
+console.log("getting message togeterh")
+console.log("a;sdfa")
+
+var message = cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+	T : "U "
+});
+
+console.log("Hello World", message);
+
+
+if (true ) {
+	console.log('sdfasdf')
+}
